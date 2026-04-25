084ed13a554b06cbc1ac2114e2ed1ce41bd228fe
dot <dot@x3c.ca>
2026-04-25 14:17:15 -0700

    strong fish billboarded

--- scripts/arm.gd
+++ scripts/arm.gd
@@ -1,86 +1,16 @@
-extends CharacterBody3D
+extends RigidBody3D
 
-const NECK_SPIN_RESPONSE = 8.0
-const NECK_SPIN_DAMPING = 6.0
-const MAX_SPIN_SPEED = 12.0
-const MIN_SCREEN_OFFSET_SQUARED = 0.0001
-
-var _spin_velocity_z: float = 0.0
-var _last_screen_offset := Vector2.ZERO
-var _has_last_screen_offset: bool = false
-
-@onready var mermaid_root: Node3D = get_parent_node_3d()
-@onready var neck: RigidBody3D = $"../RigidBody3D2"
+const ARM_MASS = 0.08
+const ARM_LINEAR_DAMP = 0.35
+const ARM_ANGULAR_DAMP = 4.5
 
 
 func _ready() -> void:
-	set_physics_process(true)
-	_sync_screen_offset()
-
-
-func _physics_process(delta: float) -> void:
-	global_position = neck.global_position
-	var camera := get_viewport().get_camera_3d()
-	if camera == null:
-		_apply_z_only_rotation(delta)
-		return
-
-	var screen_offset := _get_screen_offset(camera)
-	if screen_offset.length_squared() < MIN_SCREEN_OFFSET_SQUARED:
-		_apply_z_only_rotation(delta)
-		return
-
-	if not _has_last_screen_offset:
-		_last_screen_offset = screen_offset
-		_has_last_screen_offset = true
-		_apply_z_only_rotation(delta)
-		return
-
-	var spin_delta := _last_screen_offset.angle_to(screen_offset)
-	_last_screen_offset = screen_offset
-
-	_spin_velocity_z += spin_delta * NECK_SPIN_RESPONSE
-	_spin_velocity_z = move_toward(
-		_spin_velocity_z,
-		0.0,
-		NECK_SPIN_DAMPING * delta,
-	)
-	_spin_velocity_z = clampf(_spin_velocity_z, -MAX_SPIN_SPEED, MAX_SPIN_SPEED)
-
-	rotation = Vector3(
-		0.0,
-		0.0,
-		wrapf(rotation.z + _spin_velocity_z * delta, -PI, PI),
-	)
-
-
-func _sync_screen_offset() -> void:
-	var camera := get_viewport().get_camera_3d()
-	if camera == null:
-		return
-
-	var screen_offset := _get_screen_offset(camera)
-	if screen_offset.length_squared() < MIN_SCREEN_OFFSET_SQUARED:
-		return
-
-	_last_screen_offset = screen_offset
-	_has_last_screen_offset = true
-
-
-func _get_screen_offset(camera: Camera3D) -> Vector2:
-	var neck_screen_position := camera.unproject_position(neck.global_position)
-	var root_screen_position := camera.unproject_position(mermaid_root.global_position)
-	return neck_screen_position - root_screen_position
-
-
-func _apply_z_only_rotation(delta: float) -> void:
-	_spin_velocity_z = move_toward(
-		_spin_velocity_z,
-		0.0,
-		NECK_SPIN_DAMPING * delta,
-	)
-	rotation = Vector3(
-		0.0,
-		0.0,
-		wrapf(rotation.z + _spin_velocity_z * delta, -PI, PI),
-	)
+	mass = ARM_MASS
+	linear_damp = ARM_LINEAR_DAMP
+	angular_damp = ARM_ANGULAR_DAMP
+	gravity_scale = 1.0
+	can_sleep = false
+	contact_monitor = true
+	max_contacts_reported = 4
+	continuous_cd = true
